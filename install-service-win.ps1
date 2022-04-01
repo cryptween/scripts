81fba76468991077d44f2a9ce85c63bd0ef74d39
israel <israel.gonzalezmedina@gmail.com>
2022-04-01 10:11:56 -0300

    add windows installer first commit

---
+++ install-service-win.ps1
@@ -0,0 +1,12 @@
+# Download latest dotnet/codeformatter release from github
+
+$repo = "cryptween/scripts"
+$file = "cryptween-daemon-win.exe"
+
+$releases = "https://api.github.com/repos/$repo/releases"
+
+Write-Host Determining latest release
+$tag = (Invoke-WebRequest $releases | ConvertFrom-Json)[0].tag_name
+
+$download = "https://github.com/$repo/releases/download/$tag/$file"
+Write-Host $download
